3f376c5bfe4cb53d9b5b6caebd95a39a93c51d61
nngocjade <nngocjade@gmail.com>
2021-02-17 17:49:25 +0700

    learned expression and conditionals with react and jsx

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
+  foo = () => `Bars`;
   // render is a function within class
   //it is a lifecycle method that runs at a certain point when the component is loaded
   //it is required because it renders the output
   render() {
+    const name = "Jade Nguyen";
+    const loading = false;
+    const showName = true;
+
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
       </div>
     );
   }
 }
 
 export default App;
+
+/**
+ * JSX
+ * under the hood is actually javascript
+ *
+ */
