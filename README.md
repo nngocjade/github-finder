4b7cf9a4f0a4ce2f670473cbfdb6d421e17e52d0
nngocjade <nngocjade@gmail.com>
2021-02-18 15:03:52 +0700

    added httpRequests and updated states

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Github Finder Project
 
 Created with love by: Jade
+
+## package installed
+
+npm i axios
--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
-  foo = () => `Bars`;
+  state = {
+    users: [],
+    loading: false,
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+
+    this.setState({ users: res.data, loading: false });
+  }
 
   // render is a function within class
   //it is a lifecycle method that runs at a certain point when the component is loaded
@@ -14,7 +25,7 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
