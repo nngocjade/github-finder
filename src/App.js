c8370cbf500930491d673dfb2d227440bdde1b30
nngocjade <nngocjade@gmail.com>
2021-02-18 11:34:56 +0700

    created navbar component title&icon props and it's required prop type

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
 import "./App.css";
 
 class App extends Component {
   foo = () => `Bars`;
+
   // render is a function within class
   //it is a lifecycle method that runs at a certain point when the component is loaded
   //it is required because it renders the output
   render() {
-    const name = "Jade Nguyen";
-    const loading = false;
-    const showName = true;
-
     return (
       <div className="App">
-        {loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+        <Navbar />
       </div>
     );
   }
