1c125d177cc93f225f70e53e9bec1ba4b234cf9c
nngocjade <nngocjade@gmail.com>
2021-02-19 15:55:11 +0700

    added repo repoItem components and data

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -54,6 +55,18 @@ class App extends Component {
     this.setState({ user: res.data, loading: false });
   };
 
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    console.log("single user", res);
+
+    this.setState({ repos: res.data, loading: false });
+  };
+
   // Clear users from state
   clearUsers = () => {
     this.setState({ users: [], loading: false });
@@ -71,7 +84,8 @@ class App extends Component {
   //it is a lifecycle method that runs at a certain point when the component is loaded
   //it is required because it renders the output
   render() {
-    const { users, user, loading } = this.state;
+    // destructured
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -103,7 +117,9 @@ class App extends Component {
                   <User
                     {...props}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
                     user={user}
+                    repos={repos}
                     loading={loading}
                   />
                 )}
