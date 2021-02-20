bf33f0f44a225ad0550a5322eedf1e7a3137150b
nngocjade <nngocjade@gmail.com>
2021-02-20 13:42:19 +0700

    created reducers and actions

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,7 +1,8 @@
 import React, { useReducer } from "react";
 import axios from "axios";
-import githubContext from "./GithubContext";
-import GithubReducer from "./GithubReducer";
+import GithubContext from "./githubContext";
+import GithubReducer from "./githubReducer";
+
 import {
   SEARCH_USERS,
   SET_LOADING,
@@ -21,6 +22,17 @@ const GithubState = (props) => {
   const [state, dispatch] = useReducer(GithubReducer, initialState);
 
   // Search Users
+  const searchUsers = async (text) => {
+    setLoading();
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    dispatch({
+      type: SEARCH_USERS,
+      payload: res.data.items,
+    });
+  };
 
   // Get User
 
@@ -29,18 +41,22 @@ const GithubState = (props) => {
   // Clear Users
 
   // Set Loading
+  const setLoading = () => {
+    dispatch({ type: SET_LOADING });
+  };
 
   return (
-    <githubContext.Provider
+    <GithubContext.Provider
       value={{
         users: state.users,
         user: state.user,
         repos: state.repos,
         loading: state.loading,
+        searchUsers,
       }}
     >
       {props.children}
-    </githubContext.Provider>
+    </GithubContext.Provider>
   );
 };
 
